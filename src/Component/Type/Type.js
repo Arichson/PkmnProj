db307b1e3f0f8ef288f8086420fe04462fc09a70
Arichson <arichsonh@gmail.com>
2021-04-11 21:36:31 -0700

    color changes to type ribbon

--- src/Component/Type/Type.js
+++ src/Component/Type/Type.js
@@ -20,7 +20,7 @@ const TheTypes = ({pkDetails}) => {
             if(type === "rock")         {setColor1("lightsalmon");              return;}
             if(type === "ground")       {setColor1("rgb(158, 90, 1)");          return;}
             if(type === "poison")       {setColor1("purple");                   return;}
-            if(type === "dark")         {setColor1("black");                    return;}
+            if(type === "dark")         {setColor1("#2f504f");                  return;}
             if(type === "bug")          {setColor1("lightgreen");               return;}
             if(type === "fighting")     {setColor1("sienna");                   return;}
             if(type === "normal")       {setColor1("white");                    return;}
@@ -28,7 +28,7 @@ const TheTypes = ({pkDetails}) => {
             if(type === "flying")       {setColor1("rgb(188, 236, 255)");       return;}
             if(type === "fairy")        {setColor1("lightpink");                return;}
             if(type === "steel")        {setColor1("rgb(116, 116, 116)");       return;}
-            if(type === "dragon")       {setColor1("blue");                     return;}
+            if(type === "dragon")       {setColor1("rgb(73, 53, 160)");         return;}
             if(type === "ice")          {setColor1("rgb(0, 157, 255)");         return;}
             if(type === "ghost")        {setColor1("rgb(146, 112, 168)");       return;}
         }
@@ -42,7 +42,7 @@ const TheTypes = ({pkDetails}) => {
             if(type === "rock")         {setColor2("lightsalmon");              return;}
             if(type === "ground")       {setColor2("rgb(158, 90, 1)");          return;}
             if(type === "poison")       {setColor2("purple");                   return;}
-            if(type === "dark")         {setColor2("black");                    return;}
+            if(type === "dark")         {setColor2("#2f504f");                  return;}
             if(type === "bug")          {setColor2("lightgreen");               return;}
             if(type === "fighting")     {setColor2("sienna");                   return;}
             if(type === "normal")       {setColor2("white");                    return;}
@@ -50,7 +50,7 @@ const TheTypes = ({pkDetails}) => {
             if(type === "flying")       {setColor2("rgb(188, 236, 255)");       return;}
             if(type === "fairy")        {setColor2("lightpink");                return;}
             if(type === "steel")        {setColor2("rgb(116, 116, 116)");       return;}
-            if(type === "dragon")       {setColor2("blue");                     return;}
+            if(type === "dragon")       {setColor2("rgb(73, 53, 160)");         return;}
             if(type === "ice")          {setColor2("rgb(0, 157, 255)");         return;}
             if(type === "ghost")        {setColor2("rgb(146, 112, 168)");       return;}
         }
@@ -60,6 +60,7 @@ const TheTypes = ({pkDetails}) => {
         useEffect(()=> {
             checkColor2(type2);
         }, [type2])
+        
         return ({
             background: `linear-gradient( to right, ${color1}, ${color2})`,
             width: `50%`,
@@ -71,9 +72,14 @@ const TheTypes = ({pkDetails}) => {
             textAlign: `left`,
         })
     }
+    const fontColor = (type) => {
+        if( type === "dark" || type === "poison" || type === "dragon"){
+            return {fontColor: "white"}
+        } else { return {fontColor: "black"}}
+    }
     return (
         <div style={TypeColor(allTypes[0], allTypes[1])}>
-            <p>Type: </p> 
+            <p style={fontColor()}>Type: </p> 
             {theTypes.map((type,i) => {
                 return(
                     <div key={i}>
